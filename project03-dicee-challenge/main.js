6f6b16a1ca9e49f0e3baff9f37f688ecb69d78e9
Fernanda <fernandakagami@gmail.com>
2022-10-07 14:52:09 -0300

    Update main.js

--- project03-dicee-challenge/main.js
+++ project03-dicee-challenge/main.js
@@ -1,9 +1,9 @@
 let randomNumberOne = Math.floor(Math.random() * 6) + 1;
 let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".img1").src = `file:///D:/the-complete-2022-web-development-bootcamp/project03-dicee-challenge/images/dice${randomNumberOne}.png`;
+document.querySelector(".img1").src = `images/dice${randomNumberOne}.png`;
 
-document.querySelector(".img2").src = `file:///D:/the-complete-2022-web-development-bootcamp/project03-dicee-challenge/images/dice${randomNumberTwo}.png`;
+document.querySelector(".img2").src = `images/dice${randomNumberTwo}.png`;
 
 let heading = document.querySelector("h1");
 
