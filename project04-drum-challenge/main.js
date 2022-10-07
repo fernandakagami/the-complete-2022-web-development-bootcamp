a1c884bf670a042db63c556a46b382d93c86d870
Fernanda <fernandakagami@gmail.com>
2022-10-07 20:07:57 -0300

    update css effects

--- project04-drum-challenge/main.js
+++ project04-drum-challenge/main.js
@@ -4,13 +4,18 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHTML = this.innerHTML;
 
-        makeSound(buttonInnerHTML);        
+        makeSound(buttonInnerHTML);
 
+        buttonAnimation(buttonInnerHTML);
+        
     });
 }
 
 document.addEventListener("keypress", function(event) {
+
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 })
 
 
@@ -49,4 +54,12 @@ function makeSound(key) {
     }
 }
 
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
 
