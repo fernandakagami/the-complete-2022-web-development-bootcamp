670535a8400f403e2dab6fc5a669524137bd4490
Fernanda <fernandakagami@gmail.com>
2022-10-07 14:14:39 -0300

    fix name

--- project03-dicee-challenge/main.js
+++ project03-dicee-challenge/main.js
@@ -6,12 +6,10 @@ let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector(".img2").src = `file:///D:/the-complete-2022-web-development-bootcamp/projetc03-dicee-challenge/images/dice${randomNumberTwo}.png`;
 
-let heading = document.querySelector("h1");
-
 if (randomNumberOne > randomNumberTwo) {
-    heading = "Play 1 wins!";
+    document.querySelector("h1") = "Play 1 wins!";
 } else if (randomNumberTwo > randomNumberOne) {
-    heading = "Play 2 wins!";
+    document.querySelector("h1") = "Play 2 wins!";
 } else {
-    heading = "Draw!";
+    document.querySelector("h1") = "Draw!";
 }
