8cf8441784964104bc2862aaaab49e5d00fd2257
Fernanda <fernandakagami@gmail.com>
2022-10-07 14:45:53 -0300

    Update main.js

--- project03-dicee-challenge/main.js
+++ project03-dicee-challenge/main.js
@@ -1,15 +1,16 @@
 let randomNumberOne = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector(".img1").src = `file:///D:/the-complete-2022-web-development-bootcamp/projetc03-dicee-challenge/images/dice${randomNumberOne}.png`;
-
 let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".img2").src = `file:///D:/the-complete-2022-web-development-bootcamp/projetc03-dicee-challenge/images/dice${randomNumberTwo}.png`;
+document.querySelector(".img1").src = `file:///D:/the-complete-2022-web-development-bootcamp/project03-dicee-challenge/images/dice${randomNumberOne}.png`;
+
+document.querySelector(".img2").src = `file:///D:/the-complete-2022-web-development-bootcamp/project03-dicee-challenge/images/dice${randomNumberTwo}.png`;
+
+let heading = document.querySelector("h1");
 
 if (randomNumberOne > randomNumberTwo) {
-    document.querySelector("h1") = "Play 1 wins!";
+    document.querySelector("h1").innerHTML = "Play 1 Wins!";
 } else if (randomNumberTwo > randomNumberOne) {
-    document.querySelector("h1") = "Play 2 wins!";
+    document.querySelector("h1").innerHTML = "Play 2 Wins!";
 } else {
-    document.querySelector("h1") = "Draw!";
+    document.querySelector("h1").innerHTML = "Draw!";
 }
