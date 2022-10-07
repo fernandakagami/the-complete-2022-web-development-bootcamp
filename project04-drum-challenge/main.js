43c72bd1da336ce380a99e83244f264427cfce0a
Fernanda <fernandakagami@gmail.com>
2022-10-07 15:47:36 -0300

    Update main.js

---
+++ project04-drum-challenge/main.js
@@ -0,0 +1,9 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        alert("I got clicked!");
+    });
+}
+
+
