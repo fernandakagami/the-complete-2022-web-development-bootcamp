12e0cf3c382c8a4e1f0682e25de951297422cda0
Fernanda <fernandakagami@gmail.com>
2022-10-08 07:02:42 -0300

    create button

--- project03-dice-challenge/main.js
+++ project03-dice-challenge/main.js
@@ -1,16 +1,27 @@
-let randomNumberOne = Math.floor(Math.random() * 6) + 1;
-let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice-button").addEventListener("click", function () {
+    let randomNumberOne = Math.floor(Math.random() * 6) + 1;
+    let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector(".img1").src = `images/dice${randomNumberOne}.png`;
+    document.querySelector(".img1").src = `images/dice${randomNumberOne}.png`;
+    document.querySelector(".img2").src = `images/dice${randomNumberTwo}.png`;
 
-document.querySelector(".img2").src = `images/dice${randomNumberTwo}.png`;
+    if (randomNumberOne > randomNumberTwo) {
+        document.querySelector("h1").innerHTML = "Play 1 Wins!";
+    } else if (randomNumberTwo > randomNumberOne) {
+        document.querySelector("h1").innerHTML = "Play 2 Wins!";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+    
+});
 
-let heading = document.querySelector("h1");
+function buttonAnimation(button) {
+    var activeButton = document.querySelector("." + button);
+    activeButton.classList.add("pressed");
 
-if (randomNumberOne > randomNumberTwo) {
-    document.querySelector("h1").innerHTML = "Play 1 Wins!";
-} else if (randomNumberTwo > randomNumberOne) {
-    document.querySelector("h1").innerHTML = "Play 2 Wins!";
-} else {
-    document.querySelector("h1").innerHTML = "Draw!";
-}
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+};
+
+
