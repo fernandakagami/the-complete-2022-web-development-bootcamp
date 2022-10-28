79f49f82e5e40f04e0418acdd5e1ef71a65091d5
Fernanda <fernandakagami@gmail.com>
2022-10-28 08:44:49 -0300

    Update app.js

--- project07-weather-api/app.js
+++ project07-weather-api/app.js
@@ -13,7 +13,7 @@ app.get("/", function(req, res){
 app.post("/", function(req, res) {
  
     const query = req.body.cityName;
-    const apiKey = "bc0d319eb3cda0d1a33ab0a1f406269f";
+    const apiKey = "#";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey;
 
     https.get(url, function(response) {
