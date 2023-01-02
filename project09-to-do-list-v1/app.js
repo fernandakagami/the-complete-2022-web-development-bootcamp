4aa01e8933e23a21957644b9cfe46a2a093127ec
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-02 10:46:15 -0300

    update project

--- project09-to-do-list-v1/app.js
+++ project09-to-do-list-v1/app.js
@@ -3,10 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-    res.send("Hello");
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 0) {
+        day = "Sunday";        
+    } else if (currentDay === 1) {
+        day = "Monday";        
+    } else if (currentDay === 2) {
+        day = "Tuesday";        
+    } else if (currentDay === 3) {
+        day = "Wednesday";        
+    } else if (currentDay === 4) {
+        day = "Thursday";        
+    } else if (currentDay === 5) {
+        day = "Friday";        
+    } else {
+        day = "saturday"; 
+    }
+
+    res.render("list", {kindOfDay: day});
+
 });
 
-app.listen(300, function() {
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
