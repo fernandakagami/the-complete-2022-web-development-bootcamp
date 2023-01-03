ebe1034dc54c6cf4b46cdd0e68d64f724f8df938
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-03 10:36:47 -0300

    create list of items

--- project09-to-do-list-v1/app.js
+++ project09-to-do-list-v1/app.js
@@ -3,33 +3,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    if (currentDay === 0) {
-        day = "Sunday";        
-    } else if (currentDay === 1) {
-        day = "Monday";        
-    } else if (currentDay === 2) {
-        day = "Tuesday";        
-    } else if (currentDay === 3) {
-        day = "Wednesday";        
-    } else if (currentDay === 4) {
-        day = "Thursday";        
-    } else if (currentDay === 5) {
-        day = "Friday";        
-    } else {
-        day = "saturday"; 
-    }
+    var day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
 
 });
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
