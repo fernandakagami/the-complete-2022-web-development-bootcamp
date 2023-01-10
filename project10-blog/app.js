2217fb125cf451ae7324c16d142228867eeab93b
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-01-10 09:44:15 -0300

    create new routes

--- project10-blog/app.js
+++ project10-blog/app.js
@@ -17,18 +17,15 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});    
-})
+});
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});    
+});
 
-
-
-
-
-
-
-
-
-
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});    
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
